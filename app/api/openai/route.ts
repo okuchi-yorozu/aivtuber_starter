dbafa69cc5f3f2a6ba0a17be285d6c9f4f5a3385
億千よろず <okuchi.yorozu.official@gmail.com>
2024-06-01 19:39:36 +0900

    initial commit

---
+++ app/api/openai/route.ts
@@ -0,0 +1,30 @@
+import OpenAI from 'openai';
+import {NextRequest, NextResponse} from "next/server";
+
+const openai = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
+
+export async function POST(req: NextRequest) {
+    const {prompt, userId, messages} = await req.json();
+
+    if (!prompt || typeof prompt !== "string") {
+        return NextResponse.json({message: "Invalid prompt"}, {status: 400});
+    }
+
+    messages.push({role: "user", content: prompt});
+
+    try {
+        const chatCompletion = await openai.chat.completions.create({
+            messages,
+            model: 'gpt-4o',
+            user: userId,
+        });
+
+        const message = chatCompletion.choices[0].message;
+
+        return NextResponse.json({id: chatCompletion.id, message}, {status: 200});
+    } catch (error: any) {
+        return NextResponse.json({message: error.message}, {status: 500});
+    }
+}
---
+++ app/components/ChatGPT.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import {FormEvent, useState} from "react";
+
+type Message = {
+    role: string;
+    content: string;
+}
+export default function ChatGPT() {
+    const [prompt, setPrompt] = useState('');
+    const [response, setResponse] = useState<Message | null>(null);
+    const [userId, setUserId] = useState(null);
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+
+        console.log(messages);
+        const res = await fetch('/api/openai', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({userId, prompt, messages}),
+        });
+
+        const data = await res.json();
+        setResponse(data.message);
+
+        const tmpMessages = [...messages, data.message];
+        setMessages(tmpMessages);
+
+        if (userId === null) {
+            setUserId(data.id);
+        }
+    }
+
+    return (
+        <div>
+            <div>userId: {userId}</div>
+            <h1>AIVTuber</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <div>
+                <textarea
+                    value={prompt}
+                    onChange={(e) => setPrompt(e.target.value)}
+                    placeholder="質問を入力してください"
+                    rows={5}
+                    cols={50}
+                /></div>
+                    <button type="submit">実行</button>
+                </div>
+            </form>
+            {response && response.content && (
+                <div><h2>応答:</h2>
+                    <textarea
+                        rows={5}
+                        cols={50}
+                        value={response.content}></textarea>
+                </div>
+            )}
+        </div>
+    );
+}
